ac6fd382cadad46efc66e76967360c111fcf399a
Les <oles.timoshenko2017@yandex.ru>
2021-06-07 03:03:40 +0300

    my second commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+void input(int*, int*);
+
 int main()
 {
+	int num1, num2;
+	
+	input(&num1, &num2);
+	cout << "Числа: " << num1 <<  "\t" << num2;
 	
 	return 0;
+}
+
+void input(int *a, int *b)
+{
+	cout << "Введите число №1: ";
+	cin >> *a;
+	
+	cout << "Введите число №2: ";
+	cin >> *b;
 }
